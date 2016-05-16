75a22ad711bbf3099cfce01ee261c315afde5dcf
StephenBydawell <StephenBydawell@gmail.com>
2016-05-16 15:14:14 +0200

    Add new Dev Branch

--- Calculix_executer.py
+++ Calculix_executer.py
@@ -10,57 +10,62 @@ import subprocess as sp
 import os
 import pandas as pd
 import re
+import matplotlib.pyplot as plt
 
 from Pipe_Mesh_Function import pipe_mesh
 from Read_frd import readfrd
 from Aux_results import Aux_results
 
 length = 8
-thickness = 4
+thickness = 1
 radial = 10
 
-section = 2
+section = 0
 r = 1.4
 
-quad = 0
+quad = 1
 reduced = 0
 
 direc = "Tests/"
 
-pipe_mesh(direc,length, thickness, radial, section, r, quad, reduced)
+num_tests = 12
+Dall_max = np.zeros((num_tests))
+Von_Mises_max = np.zeros((num_tests))
 
-#meshfile = direc+"Mesh_" + str(length) + "x" + str(thickness) + "x" + str(2*radial) + "_" + elestr +interstr +".inp"
+for i in range(num_tests):
     
-#os.system("/home/spookfish/CalculiXLauncher-02/bin/ccx29 /home/spookfish/Projects/Masters/Mesh_Refinement/Tests/Test")
-os.system("/home/stephen/Documents/CalculiXLauncher-02/bin/ccx29 /home/stephen/Projects/Calculix_PipeMesher/Tests/Test")
+    thickness = i +1
 
+    pipe_mesh(direc,length, thickness, radial, section, r, quad, reduced)
+    
+    #meshfile = direc+"Mesh_" + str(length) + "x" + str(thickness) + "x" + str(2*radial) + "_" + elestr +interstr +".inp"
+        
+    #os.system("/home/spookfish/CalculiXLauncher-02/bin/ccx29 /home/spookfish/Projects/Masters/Mesh_Refinement/Tests/Test")
+    os.system("/home/stephen/Documents/CalculiXLauncher-02/bin/ccx29 /home/stephen/Projects/Calculix_PipeMesher/Tests/Test")
+    
+    
+    resultsfile = direc+'Test.frd'
+    selecttime = None
+    
+    time,disps,temps,stresses,strains = readfrd(resultsfile)
+    
+    Dall, Von_Mises, Principal = Aux_results(time,disps,temps,stresses,strains)
+    
+    
+    disps_df = pd.DataFrame(disps, columns=['Time', 'Node', 'dx', 'dy', 'dz'])
+    temps_df = pd.DataFrame(temps, columns=['Time', 'Node', 'T'])
+    stresses_df = pd.DataFrame(stresses, columns=['Time', 'Node', 'Sxx', 'Syy', 'Szz', 'Sxy', 'Sxz', 'Syz'])
+    strains_df = pd.DataFrame(strains, columns=['Time', 'Node', 'Exx', 'Eyy', 'Ezz', 'Exy', 'Exz', 'Eyz'])
+    
+    Dall_df = pd.DataFrame(Dall, columns=['Time', 'Node', 'Utot'])
+    Von_Mises_df = pd.DataFrame(Von_Mises, columns=['Time', 'Node', 'Von Mises Stress'])
+    Principal_df = pd.DataFrame(Principal, columns=['Time', 'Node', 'P1','P2','P3'])
+    
+    Dall_max[i] = Dall_df.loc[Dall_df['Time'] == 600]['Utot'].max()
+    Von_Mises_max[i] = Von_Mises_df.loc[Von_Mises_df['Time'] == 600]['Von Mises Stress'].max()
+    
+   # Sxx_max =  stresses_df.loc[stresses_df['Time'] == 600]['Sxx'].max()
+    #Sxx_min =  stresses_df.loc[stresses_df['Time'] == 600]['Sxx'].min()
+    
+plt.plot(Von_Mises_max)
 
-resultsfile = direc+'Test.frd'
-selecttime = None
-
-time,disps,temps,stresses,strains = readfrd(resultsfile)
-
-Dall, Von_Mises, Principal = Aux_results(time,disps,temps,stresses,strains)
-
-
-disps_df = pd.DataFrame(disps, columns=['Time', 'Node', 'dx', 'dy', 'dz'])
-temps_df = pd.DataFrame(temps, columns=['Time', 'Node', 'T'])
-stresses_df = pd.DataFrame(stresses, columns=['Time', 'Node', 'Sxx', 'Syy', 'Szz', 'Sxy', 'Sxz', 'Syz'])
-strains_df = pd.DataFrame(strains, columns=['Time', 'Node', 'Exx', 'Eyy', 'Ezz', 'Exy', 'Exz', 'Eyz'])
-
-Dall_df = pd.DataFrame(Dall, columns=['Time', 'Node', 'Utot'])
-Von_Mises_df = pd.DataFrame(Von_Mises, columns=['Time', 'Node', 'Von Mises Stress'])
-Principal_df = pd.DataFrame(Principal, columns=['Time', 'Node', 'P1','P2','P3'])
-
-Dall_max = Dall_df.loc[Dall_df['Time'] == 600]['Utot'].max()
-Von_Mises_max = Von_Mises_df.loc[Von_Mises_df['Time'] == 600]['Von Mises Stress'].max()
-
-Sxx_max =  stresses_df.loc[stresses_df['Time'] == 600]['Sxx'].max()
-Sxx_min =  stresses_df.loc[stresses_df['Time'] == 600]['Sxx'].min()
-
-
-
-
-#stresses = np.array(stresses)
-#
-#max(stresses[-1])
